e186ba6281f43d88897aab7049dead11a562936f
obioguh <93559254+obioguh@users.noreply.github.com>
2021-11-17 22:47:45 -0500

    Update project2.s

--- project2.s
+++ project2.s
@@ -169,6 +169,15 @@ asciiConverter:
     and $t0, $t0, $t1
     bne $t0, $zero, upperLetter
     
+    li $t3, 48
+    li $t4, 58
+    sgt $t0, $s7, $t3
+    slt $t1, $s7, $t4
+    and $t0, $t0, $t1
+    bne $t0, $zero, number
+    
+    j invalid
+    
     
 
 
